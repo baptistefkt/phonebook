7382fc5a1c78904e7bfca0e7db688ab7756f9c44
Baptiste Firket <baptistefirket@gmail.com>
2019-03-26 21:15:57 +0100

    add form (add + update) + style

--- client/src/pages/Add.js
+++ client/src/pages/Add.js
@@ -2,37 +2,94 @@ import React from 'react';
 import styled from 'styled-components';
 import Layout from '../components/layout';
 
-const PageContainer = styled.div`
+const PageContainer = styled.section`
   width: 90%;
+  height: 100vh;
+  max-width: 500px;
   margin: 0 auto;
+
+  h1 {
+    color: #fff;
+    font-size: 30px;
+  }
+
+  form {
+    color: #444;
+    padding: 30px 30px 50px 30px;
+    background-color: #fff;
+    border-radius: 4px;
+    box-shadow: 0 16px 40px rgba(0, 0, 0, 0.12);
+
+    input {
+      width: 90%;
+      margin: 15px 0 20px 0;
+      padding: 10px;
+      border: 1px solid #ebebeb;
+      border-radius: 4px;
+      color: #444;
+      font-size: 12px;
+
+      &:focus {
+        outline: none;
+        border: 1px solid #00b4db;
+      }
+    }
+
+    label {
+      font-weight: bold;
+      font-size: 13px;
+      text-transform: uppercase;
+      display: block;
+    }
+
+    button {
+      width: 90%;
+      margin: 0 auto;
+      padding: 12px;
+      color: #00b4db;
+      font-weight: bold;
+      font-size: 11px;
+      text-transform: uppercase;
+      border: 2px solid #00b4db;
+      background-color: transparent;
+      border-radius: 4px;
+      margin-top: 20px;
+
+      &:hover {
+        opacity: 0.7;
+        cursor: pointer;
+      }
+    }
+  }
+
+  @media (min-width: 500px) {
+    width: 80%;
+  }
+
+  @media (min-width: 900px) {
+    width: 60%;
+  }
 `;
 
-const Form = styled.form`
-  padding: 20px 30px;
-  background-color: #eee;
-`;
-
-const Add = () => (
-  <Layout>
+const Add = props => (
+  <Layout path={props.match.path}>
     <PageContainer>
-      <h1>Add new entry</h1>
-      <Form action="">
+      <h1>Create new entry</h1>
+      <form action="">
         <div>
-          <label for="firstName">First name:</label>
+          <label htmlFor="firstName">First name</label>
           <input type="text" id="firstName" name="firstName" />
         </div>
         <div>
-          <label for="lastName">Last name:</label>
+          <label htmlFor="lastName">Last name</label>
           <input type="text" id="lastName" name="lastName" />
         </div>
         <div>
-          <label for="phone">Phone number:</label>
+          <label htmlFor="phone">Phone number</label>
           <input type="text" id="phone" name="phone" />
         </div>
-        <div class="button">
-          <button type="submit">Send your message</button>
-        </div>
-      </Form>
+        <button type="submit">+ Add entry</button>
+      </form>
     </PageContainer>
   </Layout>
 );
--- client/src/pages/Update.js
+++ client/src/pages/Update.js
@@ -1,9 +1,97 @@
 import React from 'react';
+import styled from 'styled-components';
 import Layout from '../components/layout';
 
-const Update = () => (
-  <Layout>
-    <h1>Update</h1>
+const PageContainer = styled.section`
+  width: 90%;
+  height: 100vh;
+  max-width: 600px;
+  margin: 0 auto;
+
+  h1 {
+    color: #fff;
+    font-size: 30px;
+  }
+
+  form {
+    color: #333;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 4px;
+
+    input {
+      width: 50%;
+      margin: 10px 0 15px 0;
+      padding: 10px;
+      border: 1px solid #ebebeb;
+      border-radius: 4px;
+      color: #444;
+      font-size: 12px;
+
+      &:focus {
+        outline: none;
+        border: 1px solid #00b4db;
+      }
+    }
+
+    label {
+      font-weight: bold;
+      font-size: 12px;
+      text-transform: uppercase;
+      display: block;
+    }
+
+    button {
+      width: 90%;
+      margin: 0 auto;
+      padding: 12px;
+      color: #00b4db;
+      font-weight: bold;
+      font-size: 11px;
+      text-transform: uppercase;
+      border: 2px solid #00b4db;
+      background-color: transparent;
+      border-radius: 4px;
+      margin-top: 20px;
+
+      &:hover {
+        opacity: 0.7;
+        cursor: pointer;
+      }
+    }
+  }
+
+  @media (min-width: 500px) {
+    width: 80%;
+  }
+
+  @media (min-width: 900px) {
+    width: 60%;
+  }
+`;
+
+const Update = props => (
+  <Layout path={props.match.path}>
+    <PageContainer>
+      <h1>Edit this entry</h1>
+      <form action="">
+        <div>
+          <label for="firstName">First name</label>
+          <input type="text" id="firstName" name="firstName" />
+        </div>
+        <div>
+          <label for="lastName">Last name</label>
+          <input type="text" id="lastName" name="lastName" />
+        </div>
+        <div>
+          <label for="phone">Phone number</label>
+          <input type="text" id="phone" name="phone" />
+        </div>
+        <div class="button">
+          <button type="submit">Update entry</button>
+        </div>
+      </form>
+    </PageContainer>
   </Layout>
 );
 
